900a35ed651615660b6b418fa5c272f1a2faaeef
appleRtsan <45231395+appleRtsan@users.noreply.github.com>
2020-09-13 16:39:39 +0800

    Update crawl.py
    把爬蟲補完了  欠了超久的債

--- crawl.py
+++ crawl.py
@@ -6,15 +6,32 @@ def crawl(URL,time):
     url=requests.get(URL)#url的type是 response status code  url.text才是裡面的HTML檔
     soup=BeautifulSoup(url.text,"html.parser")#html.parser是把url.text的HTML檔當字串
     data=soup.select("body .r-ent .title a")
+    for j in data:
+        print(j)
     findhref=soup.select(".btn-group-paging .wide")
+    #print("finding href:")
     count=0
-    for f in findhref:
+    for j in findhref:
+        if count == 1:
+            nextpage = j;
+      #      print(nextpage);
+       # print(j)
+        count=count+1;
+    nexturl=str(nextpage)
+    count=0;
+    NU=str('')
+    for n in nexturl:
         count=count+1
-        if count == 2:
-            print(f)
-            nexthref=f
-            N=nexthref.a['href']
-            print(N)
+        if count < 58 and count > 26:
+            NU=NU+n
+            #print(NU)
+    nexturl="https://www.ptt.cc"+str(NU)
+    print("nexturl=")
+    print(nexturl)
+    time=time-1;
+    crawl(nexturl,time)
 #    for d in data:
 #        print(d,end="\n")
+
+
 crawl("https://www.ptt.cc/bbs/StupidClown/index.html",3)
